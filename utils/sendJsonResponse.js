2c3f782f6c33f76ddca0cd9f06e01d75cbb1a45b
Rohan Kiran Adhav <155009973+Rohan-Adhav@users.noreply.github.com>
2025-08-24 17:15:06 +0530

    Update sendJsonResponse.js

--- utils/sendJsonResponse.js
+++ utils/sendJsonResponse.js
@@ -3,6 +3,6 @@ export default function sendJsonResponse(res, statusCode, payLoad) {
     res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.statusCode = statusCode
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(payLoad))
+    res.end(JSON.stringify(payLoad,null, 2))
 
 }
